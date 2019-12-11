c15a273ca91196bd555d5c594ade60a9e10217b2
Bhanuka Gamage <bhanukamgamage@gmail.com>
2019-12-11 09:01:23 +0800

    Used codable to save persistant data

--- Section 19/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
+++ Section 19/Todoey-iOS13/Todoey/Controllers/TodoListViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Eggs", "Destro Demogorgon"]
+    var itemArray = [Item]()
+    
+    
+    // let defaults = UserDefaults.standard
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadItems()
+        
+//        if let items = defaults.array(forKey : "TodoListArray") as? [Item] {
+//            itemArray = items
+//        }
+        
     }
 
     /// MARK: - TableView Datasource Methods
@@ -25,14 +38,24 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
 
     /// MARK: - Table View Delegate Method
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
+
+        
+        let item = itemArray[indexPath.row]
+        
+        item.done = !item.done
+        
+        saveItems()
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
@@ -40,7 +63,10 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
+        tableView.reloadData()
+        
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
     /// MARK: - Add new items
@@ -54,7 +80,13 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // When user clicks the Add Item
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
+            // self.defaults.set(self.itemArray, forKey : "TodoListArray")
             
             self.tableView.reloadData()
             
@@ -69,6 +101,36 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            
+            try data.write(to:dataFilePath!)
+            
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+    }
+    
+    
+    func loadItems() {
+        
+        
+        if let data = try? Data(contentsOf : dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+            
+        }
     }
     
     
---
+++ Section 19/Todoey-iOS13/Todoey/Models/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Bhanuka Gamage on 12/11/19.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+class Item : Codable {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
