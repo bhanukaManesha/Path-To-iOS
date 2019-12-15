3b0bf0136de4da0019cfabc73be0a01844ae18b6
Bhanuka Gamage <bhanukamgamage@gmail.com>
2019-12-15 16:46:51 +0800

    Initialize Section 23

---
+++ Section 23/SeeFood/SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Bhanuka Gamage on 12/15/19.
+//  Copyright © 2019 Bhanuka Gamage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
